c06629cf9c598df0672ded0d0955a69c3e012115
Matteo Ferrando <matteo.ferrando2@gmail.com>
2013-11-18 10:23:01 -0500

    assignment 5

---
+++ ass5/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ass5::Application.config.secret_key_base = '634569d43513cdcbd6a56dbce1eaf0ecbaad94955084e97589d2f020ee8815a70d8f89f3d0a0041c94a472c7a3c61bef6fe5383683296dc68ef81d6c04af7f6f'
